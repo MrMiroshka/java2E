1aed5ffd7b6182794b61f369402a37148bda9b06
Miroshnichenko Igor <Miroshnichenko.Igor.Valerivich@yandex.ru>
2022-04-26 15:11:12 +0500

    Home Work 6 Все по заданию. Ничего лишнего

--- src/main/java/homework6/Client.java
+++ src/main/java/homework6/Client.java
@@ -17,57 +17,57 @@ public class Client {
         client.start();
     }
 
-    private void start(){
-        try{
+    private void start() {
+        try {
             openConnection();
             final Scanner scanner = new Scanner(System.in);
-            while(true) {
+            while (true) {
                 final String msg = scanner.nextLine();
                 outputStream.writeUTF(msg);
-                 if ("/end".equalsIgnoreCase(msg)){
+                if ("/end".equalsIgnoreCase(msg)) {
                     break;
                 }
             }
-        }catch (Exception exp){
+        } catch (Exception exp) {
             exp.printStackTrace();
         }
     }
 
     private void closeConnection() {
-        if (socket != null){
+        if (socket != null) {
             try {
                 socket.close();
-            }catch (Exception exp){
+            } catch (Exception exp) {
                 exp.printStackTrace();
             }
         }
 
-        if (inputStream !=null){
+        if (inputStream != null) {
             try {
                 inputStream.close();
-            }catch (Exception exp){
+            } catch (Exception exp) {
                 exp.printStackTrace();
             }
         }
 
-        if (outputStream !=null){
+        if (outputStream != null) {
             try {
                 outputStream.close();
-            }catch (Exception exp){
+            } catch (Exception exp) {
                 exp.printStackTrace();
             }
         }
     }
 
-    private  void openConnection() throws IOException {
-        socket = new Socket("localhost",8386);
+    private void openConnection() throws IOException {
+        socket = new Socket("localhost", 8386);
         inputStream = new DataInputStream(socket.getInputStream());
         outputStream = new DataOutputStream(socket.getOutputStream());
         final Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
-                    while(true) {
+                    while (true) {
                         final String msg = inputStream.readUTF();
                         if ("/end".equalsIgnoreCase(msg)) {
                             break;
@@ -76,7 +76,7 @@ public class Client {
                     }
                 } catch (IOException e) {
                     System.out.println("Соединение с сервером было разорвано!");
-                }finally {
+                } finally {
                     closeConnection();
                 }
             }
--- src/main/java/homework6/Server.java
+++ src/main/java/homework6/Server.java
@@ -1,34 +1,106 @@
 package homework6;
 
-import javax.swing.table.TableRowSorter;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Server {
+    private ServerSocket serverSocket;
+    private Socket socket;
+    private DataInputStream inputStream;
+    private DataOutputStream outputStream;
+
     public static void main(String[] args) {
-        //Socket socket = null;
-        try(ServerSocket serverSocket = new ServerSocket(8386)) {// try with resources
-            System.out.println("Сервер запущен, ожидаем подключения...");
-            Socket socket = serverSocket.accept(); //блокирующий метод (на подобие join)
-            System.out.println("Клиент подключился");
-            final DataInputStream inputStream = new DataInputStream(socket.getInputStream());
-            final DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
-            while(true){
-                final String msg = inputStream.readUTF();//блокирующий метод (на подобие join)
-                System.out.println("Сообщение от клиента: " + msg );
+        final Server server = new Server();
+        server.start();
+
+    }
+
+    private void start() {
+        try {// try with resources
+
+            openConnection();
+
+            final Scanner scanner = new Scanner(System.in);
+            while (true) {
+                final String msg = scanner.nextLine();
+                outputStream.writeUTF(msg);
                 if ("/end".equalsIgnoreCase(msg)) {
-                    outputStream.writeUTF("/end");
+                    closeConnection();
                     break;
                 }
-                outputStream.writeUTF("Сообщение от клиента:" + msg);
             }
-        }catch ( IOException exp){
+        } catch (IOException exp) {
             exp.printStackTrace();
             throw new RuntimeException(exp);
+        } catch (Exception exp) {
+            exp.printStackTrace();
         }
+
         System.out.println("Сервер остановлен");
+
+
+    }
+
+
+
+
+    private void openConnection() throws IOException {
+        serverSocket = new ServerSocket(8386);
+        System.out.println("Сервер запущен, ожидаем подключения...");
+        socket = serverSocket.accept(); //блокирующий метод (на подобие join)
+        System.out.println("Клиент подключился");
+        inputStream = new DataInputStream(socket.getInputStream());
+        outputStream = new DataOutputStream(socket.getOutputStream());
+
+        final Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    while (true) {
+                        final String msg = inputStream.readUTF();//блокирующий метод (на подобие join)
+                        System.out.println("Сообщение от клиента: " + msg);
+                        if ("/end".equalsIgnoreCase(msg)) {
+                            outputStream.writeUTF("/end");
+                            break;
+                        }
+                    }
+                } catch (IOException e) {
+                    System.out.println("Соединение с сервером было разорвано!");
+                } finally {
+                    closeConnection();
+                }
+            }
+        });
+        thread.start();
+    }
+
+    private void closeConnection() {
+        if (socket != null) {
+            try {
+                socket.close();
+            } catch (Exception exp) {
+                exp.printStackTrace();
+            }
+        }
+
+        if (inputStream != null) {
+            try {
+                inputStream.close();
+            } catch (Exception exp) {
+                exp.printStackTrace();
+            }
+        }
+
+        if (outputStream != null) {
+            try {
+                outputStream.close();
+            } catch (Exception exp) {
+                exp.printStackTrace();
+            }
+        }
     }
 }
